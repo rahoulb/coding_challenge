cd63465e3d55658db119443ea397da2458f69271
Rahoul Baruah <rahoulb@echodek.co>
2021-03-22 15:49:19 +0100

    Checkout behaves as required

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -9,11 +9,14 @@ class Checkout
   end
 
   def total
-    sub_total = @scanned_items.inject(0.0) do |sub_total, item|
+    items = @rules&.adjust_prices_within(@scanned_items) || @scanned_items
+
+    sub_total = items.inject(0.0) do |sub_total, item|
       sub_total += item.price
     end
     return sub_total if @rules.nil?
-    percentage_discount = @rules.calculate_discount_for(@scanned_items)
+
+    percentage_discount = @rules.calculate_discount_for(items) || 0.0
     return (sub_total * (100.0 - percentage_discount)/100.0)
   end
 
--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/checkout'
 describe Checkout do
   let(:first_product) { stub code: '001', description: 'First', price: 2.5 }
   let(:second_product) { stub code: '002', description: 'Second', price: 3.5 }
+  let(:second_product_with_adjusted_price) { stub code: '002', description: 'Second', price: 3.0 }
   let(:third_product) { stub code: '003', description: 'Third', price: 8.0 }
 
   describe "calculating prices" do
@@ -22,23 +23,38 @@ describe Checkout do
 
   describe "discounts" do
     subject { Checkout.new rules }
-    let(:rules) { stub }
+    let(:rules) { stub_everything }
     let(:items) { [first_product, third_product] }
 
     before do
-      rules.expects(:calculate_discount_for).with(items).returns(10)
+      rules.expects(:calculate_discount_for).returns(10)
     end
 
     it "applies a 10% discount" do
       subject.scan first_product
       subject.scan third_product
-      # 2.5 + 8 = 10.5 
+      # 2.5 + 8 = 10.5
       # with a 10% discount = 9.45
 
       _(subject.total).must_equal 9.45
     end
-
   end
 
+  describe "special offer prices" do
+    subject { Checkout.new rules }
+    let(:rules) { stub_everything }
+    let(:items) { [second_product, second_product] }
+    let(:adjusted_items) { [second_product_with_adjusted_price, second_product_with_adjusted_price] }
 
+    before do
+      rules.expects(:adjust_prices_within).returns(adjusted_items)
+    end
+
+    it "uses special offer pricing" do
+      subject.scan second_product
+      subject.scan second_product
+
+      _(subject.total).must_equal 6.0
+    end
+  end
 end
