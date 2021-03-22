cfac8e6eab43bef72cfe2057939bbd64939a0b1e
Rahoul Baruah <rahoulb@echodek.co>
2021-03-22 16:09:12 +0100

    Calculate sub totals

--- lib/promotional_rules.rb
+++ lib/promotional_rules.rb
@@ -3,4 +3,11 @@ class PromotionalRules
     sub_total = sub_total.to_f
     (sub_total >= 60.0) ? 10.0 : 0.0
   end
+
+  def calculate_total_for items
+    return 0.0 if items.nil?
+    items.inject(0.0) do | t, i |
+      t += i.price.to_f
+    end
+  end
 end
--- test/promotional_rules_test.rb
+++ test/promotional_rules_test.rb
@@ -4,6 +4,16 @@ require_relative '../lib/promotional_rules'
 
 describe PromotionalRules do
   subject { PromotionalRules.new }
+  let(:first_product) { stub code: '001', description: 'First', price: 2.5 }
+  let(:second_product) { stub code: '002', description: 'Second', price: 3.5 }
+  let(:third_product) { stub code: '003', description: 'Third', price: 8.0 }
+  let(:items) { [first_product, second_product, third_product] }
+
+  describe "calculating sub-total" do
+    it "is the sum of the products supplied" do
+      _(subject.calculate_total_for(items)).must_equal 14.0
+    end
+  end
   describe "discount" do
     it "of 10% is applied if the total is equal to or higher than 60" do
       _(subject.calculate_discount_for(60.0)).must_equal 10.0
